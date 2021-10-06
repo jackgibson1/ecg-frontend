811feff0eb4563b5f9c907dfab157b320a34826d
Jack Gibson <jgibson49@qub.ac.uk>
2021-10-06 22:26:19 +0100

    Configuring eslint scripts to adhere to JS Airbnb style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
       <h1>Electrocardiography Web Application</h1>
-      
     </div>
   );
 }
