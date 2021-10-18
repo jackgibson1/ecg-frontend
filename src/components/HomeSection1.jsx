4640e9d3d6e668760075e4e1a70300a9a48531d9
Jack Gibson <jgibson49@qub.ac.uk>
2021-10-18 12:32:57 +0100

    Animated heart now displaying - looks good

--- src/components/HomeSection1.jsx
+++ src/components/HomeSection1.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Typography, Grid, Box } from '@mui/material';
+import animatedImage from '../assets/images/animatedGif1.gif';
 
 function HomeSection1() {
   return (
     <>
-      <Grid item xs={12}>
-        <Box sx={{ padding: '2%' }}>
+      <Grid item xs={12} md={6} lg={6} spacing={2}>
+        <Box sx={{ paddingLeft: '5%', paddingRight: '5%', paddingTop: '10%' }}>
           <Typography variant="h2" component="div">
-            <b>Learn interactively</b>
+            <b>
+              Learn
+              {' '}
+              <br />
+              {' '}
+              interactively
+            </b>
           </Typography>
-          <Typography varaint="body1">
+          <Typography variant="h5">
             Replace lecture videos with hands-on, interactive practice.
+            <br />
             Learn electrocardiography the right way.
           </Typography>
         </Box>
       </Grid>
-      <Grid item xs={12}>
-        <h6>image</h6>
+      <Grid item xs={12} md={6} lg={6} spacing={1}>
+        <Box sx={{ paddingLeft: '5%', paddingRight: '5%', paddingTop: '10%' }}>
+          <img src={animatedImage} alt="animated heart" />
+        </Box>
       </Grid>
     </>
   );
