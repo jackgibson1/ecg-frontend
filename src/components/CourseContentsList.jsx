e56042c1f89ff0e38bc6e42a64b0fad2fab1d7a3
40231676 <jgibson49@qub.ac.uk>
2022-01-07 11:51:38 +0000

    working on anatomy course

--- src/components/CourseContentsList.jsx
+++ src/components/CourseContentsList.jsx
@@ -6,19 +6,16 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
-import courseDetails from './courseDetails';
 
 export default function CourseContentsList(props) {
   // eslint-disable-next-line react/prop-types
-  const { pathname } = props;
-
-  const course = courseDetails.find((cse) => cse.path === pathname);
-  console.log(course);
+  const { sections } = props;
 
   return (
     <nav aria-label="main mailbox folders">
       <List>
-        {course.sections.map((section) => (
+        {/* eslint-disable-next-line react/prop-types */}
+        {sections.map((section) => (
           <>
             <ListItem disablePadding>
               <ListItemButton>
@@ -30,9 +27,7 @@ export default function CourseContentsList(props) {
             </ListItem>
             <Divider />
           </>
-
         ))}
-
       </List>
     </nav>
   );
--- src/components/CourseLayout.jsx
+++ src/components/CourseLayout.jsx
@@ -4,36 +4,56 @@ import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { ButtonGroup, Button } from '@mui/material';
+import courseDetails from './courseDetails';
 import CourseContentsList from './CourseContentsList';
+import CourseBackground from '../assets/images/courses/coursebackground.jpeg';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  height: '500%',
+  height: '500px',
+}));
+
+const NavigationArea = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: '10%',
+  marginTop: '1%',
 }));
 
 export default function CourseLayout(props) {
   // eslint-disable-next-line react/prop-types
   const { pathname } = props.location;
 
+  const course = courseDetails.find((cse) => cse.path === pathname);
+
   return (
     <Grid sx={{ paddingTop: '2%', paddingLeft: '2%', paddingRight: '2%' }} fluid container justify="center">
       <Grid item xs={2}>
         <Item>
-          <CourseContentsList pathname={pathname} />
+          <CourseContentsList sections={course.sections} />
         </Item>
       </Grid>
       <Grid item xs={10}>
         <Item>
           <Box sx={{
-            borderColor: 'grey.500', border: 5, borderRadius: 5, width: '100%', height: '100%',
+            borderColor: 'grey.500', border: 5, borderRadius: 5, width: '100%', height: '100%', backgroundImage: `url(${CourseBackground})`, backgroundSize: 'cover',
           }}
           >
-            Course Components Iterated Here
+            {course.components[0]}
           </Box>
         </Item>
+        <NavigationArea>
+          <ButtonGroup disableElevation variant="contained">
+            <Button>Previous</Button>
+            <Button>Next</Button>
+          </ButtonGroup>
+        </NavigationArea>
       </Grid>
     </Grid>
   );
