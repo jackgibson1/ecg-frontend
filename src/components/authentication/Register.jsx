9640da2366a072d7ac79723e22f87b5e3436ae57
40231676 <jgibson49@qub.ac.uk>
2022-01-31 18:55:01 +0000

    register section tidy up

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-import CheckButton from 'react-validation/build/button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
@@ -13,44 +12,27 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Alert from '@mui/material/Alert';
 import { isEmail } from 'validator';
+import TextField from '@mui/material/TextField';
+import { Stack } from '@mui/material';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 import AuthService from '../../services/auth.service';
 
-const required = (value) => {
-  if (!value) {
-    return (
-      <Alert severity="error">This field is required!</Alert>
-    );
+function checkAllValid(username, email, password, setFieldValid) {
+  if (!isEmail(email)) {
+    setFieldValid((prevField) => ({ ...prevField, emailValid: false }));
+    return { valid: false, message: 'Please enter a valid email address' };
   }
-  return null;
-};
-
-const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <Alert severity="error">This is not a valid email!</Alert>
-    );
+  if (username.length < 3 || username.length > 20) {
+    setFieldValid((prevField) => ({ ...prevField, nameValid: false }));
+    return { valid: false, message: 'The username must be between 3 and 20 characters.' };
   }
-  return null;
-};
-
-const validUsername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <Alert severity="error">The username must be between 3 and 20 characters.</Alert>
-    );
+  if (password.length < 6 || password.length > 40) {
+    setFieldValid((prevField) => ({ ...prevField, passValid: false }));
+    return { valid: false, message: 'The password must be between 6 and 40 characters.' };
   }
-  return null;
-};
-
-const validPassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <Alert severity="error">The password must be between 6 and 40 characters.</Alert>
-    );
-  }
-  return null;
-};
+  return { valid: true };
+}
 
 function Copyright(props) {
   return (
@@ -68,18 +50,19 @@ function Copyright(props) {
 }
 
 const Register = (props) => {
-  const form = useRef();
-  const checkBtn = useRef();
-
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [successful, setSuccessful] = useState(false);
   const [message, setMessage] = useState('');
+  const [fieldValid, setFieldValid] = useState(
+    { nameValid: true, emailValid: true, passValid: true },
+  );
 
   const onChangeUsername = (e) => {
     const usernameTarget = e.target.value;
     setUsername(usernameTarget);
+    setFieldValid((prevField) => ({ ...prevField, nameValid: true }));
   };
 
   const onChangeEmail = (e) => {
@@ -98,34 +81,37 @@ const Register = (props) => {
     setMessage('');
     setSuccessful(false);
 
-    form.current.validateAll();
+    const isValid = checkAllValid(username, email, password, setFieldValid);
+    if (!isValid.valid) {
+      setSuccessful(false);
+      setMessage(isValid.message);
+      return;
+    }
 
     // eslint-disable-next-line no-underscore-dangle
-    if (checkBtn.current.context._errors.length === 0) {
-      AuthService.register(username, email, password).then(
-        // eslint-disable-next-line no-unused-vars
-        async () => {
-          setMessage('Successfully registered');
-          setSuccessful(true);
+    AuthService.register(username, email, password).then(
+      // eslint-disable-next-line no-unused-vars
+      async () => {
+        setMessage('Successfully registered');
+        setSuccessful(true);
 
-          await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 3000));
 
-          // eslint-disable-next-line react/prop-types
-          props.history.push('/courses');
-          window.location.reload();
-        },
-        (error) => {
-          const resMessage = (error.response
+        // eslint-disable-next-line react/prop-types
+        props.history.push('/courses');
+        window.location.reload();
+      },
+      (error) => {
+        const resMessage = (error.response
                         && error.response.data
                         && error.response.data.message)
                         || error.message
                         || error.toString();
 
-          setMessage(resMessage);
-          setSuccessful(false);
-        },
-      );
-    }
+        setMessage(resMessage);
+        setSuccessful(false);
+      },
+    );
   };
 
   return (
@@ -139,74 +125,49 @@ const Register = (props) => {
           alignItems: 'center',
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <img
-            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-            alt="profile-img"
-            className="profile-img-card"
-          />
-        </Avatar>
+        <PersonAddAltIcon sx={{ transform: 'scale(3)', marginBottom: '5%' }} />
         <Typography component="h1" variant="h5">
           Sign Up
         </Typography>
 
-        <Form onSubmit={handleRegister} ref={form}>
+        <Form onSubmit={handleRegister}>
           {!successful && (
-          <div>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <Input
-                type="text"
-                className="form-control"
-                name="username"
-                value={username}
-                onChange={onChangeUsername}
-                validations={[required, validUsername]}
-              />
-            </div>
+          <Stack spacing={2} sx={{ marginTop: '5%' }}>
+            <TextField
+              required
+              label="Username"
+              onChange={onChangeUsername}
+              error={!fieldValid.nameValid}
+            />
+            <TextField
+              required
+              label="Email"
+              onChange={onChangeEmail}
+              error={!fieldValid.emailValid}
+            />
 
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <Input
-                type="text"
-                className="form-control"
-                name="email"
-                value={email}
-                onChange={onChangeEmail}
-                validations={[required, validEmail]}
-              />
-            </div>
+            <TextField
+              required
+              label="Password"
+              onChange={onChangePassword}
+              error={!fieldValid.passValid}
+              type="password"
+            />
 
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <Input
-                type="password"
-                className="form-control"
-                name="password"
-                value={password}
-                onChange={onChangePassword}
-                validations={[required, validPassword]}
-              />
-            </div>
-
-            <div className="form-group">
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-            </div>
-          </div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+          </Stack>
           )}
-
-          {message && (
-          <Alert severity={successful ? 'success' : 'error'}>{message}</Alert>
-          )}
-          <CheckButton style={{ display: 'none' }} ref={checkBtn} />
         </Form>
+        {message && (
+          <Alert sx={{ marginTop: '3%' }} severity={successful ? 'success' : 'error'}>{message}</Alert>
+        )}
       </Box>
       <Copyright sx={{ mt: 8, mb: 4 }} />
     </Container>
