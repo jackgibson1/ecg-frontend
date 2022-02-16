37e21a7e38b1adb7a668b2e7a8aa0cbae0bdef6c
40231676 <jgibson49@qub.ac.uk>
2022-02-16 21:29:00 +0000

    creating admin services

---
+++ src/services/admin.service.js
@@ -0,0 +1,22 @@
+/*
+ * Admin services for interacting with the API
+ * get all users, delete user, reset all stats for user, delete post and delete comment
+*/
+import axios from 'axios';
+import authService from './auth.service';
+import authHeader from './auth-header';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+function getAllUsers() {
+  return axios.get(`${API_URL}/admin/all-users`,
+    { headers: {
+      'x-access-token': authHeader()['x-access-token'],
+      'admin-user-id': authService.getCurrentUser().id,
+    },
+    });
+}
+
+export default {
+  getAllUsers,
+};
