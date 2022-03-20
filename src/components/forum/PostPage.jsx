dfa3e1387fe14df0ee15246888f5ef0e0eb600fb
40231676 <jgibson49@qub.ac.uk>
2022-03-20 22:05:42 +0000

    continuing to style post page - next step is to show comments

--- src/components/forum/PostPage.jsx
+++ src/components/forum/PostPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import moment from 'moment';
+import CommentIcon from '@mui/icons-material/Comment';
 import ForumService from '../../services/forum.service';
 
 export default function PostPage(props) {
@@ -18,23 +19,23 @@ export default function PostPage(props) {
 
   return (
     <Box sx={{
-      marginTop: 3,
+      marginTop: 2,
       marginBottom: 3,
       display: 'flex',
       flexDirection: 'column',
       marginLeft: 'auto',
       marginRight: 'auto',
-      width: '90%',
+      width: '95%',
       justifyContent: 'center',
       maxWidth: '1500px',
     }}
     >
-      <div style={{ marginTop: '5px', marginLeft: '20px', display: 'inline' }}>
-        <Typography sx={{ display: 'inline', color: 'text.primary' }} variant="body2">
+      <div style={{ marginLeft: '20px', display: 'inline' }}>
+        <Typography sx={{ display: 'inline', color: 'text.primary' }} variant="body1">
           {postData.username}
         </Typography>
-        <Typography sx={{ display: 'inline', color: 'text.secondary' }} variant="body2">
-          {' '} asked {moment(postData.date).fromNow()}.
+        <Typography sx={{ display: 'inline', color: 'text.secondary' }} variant="body1">
+          {' '} asked {moment(postData.date).fromNow()}:
         </Typography>
       </div>
       <Box sx={{ backgroundColor: '#E8E6E6', marginLeft: 'auto', marginRight: 'auto', width: '98%', borderRadius: 2, marginTop: '10px' }}>
@@ -55,10 +56,18 @@ export default function PostPage(props) {
           <Typography variant="body" sx={{ display: 'block', textDecoration: 'underline' }}>
             Attached Image:
           </Typography>
-          <img style={{ maxWidth: '1200px', maxHeight: '1000px', marginTop: '10px', borderRadius: 10 }} src={`${process.env.REACT_APP_IMAGES_URL}/${postImgName}`} alt="user uplaoded" />
+          <img style={{ maxWidth: '1200px', maxHeight: '1000px', marginTop: '10px', marginBottom: '10px', borderRadius: 10 }} src={`${process.env.REACT_APP_IMAGES_URL}/${postImgName}`} alt="user uplaoded" />
         </Box>
         )}
       </Box>
+      <div style={{ marginLeft: '20px', display: 'inline', marginTop: '10px' }}>
+        <Box sx={{ display: 'flex' }}>
+          <CommentIcon />
+          <Box>
+            <Typography sx={{ ml: 1 }} variant="body1">4 Total Comments:</Typography>
+          </Box>
+        </Box>
+      </div>
     </Box>
 
   );
